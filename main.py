916bdaa643ad9c5548ecf40b17c82118be8ea874
charlie <dev.charlieaprilianto@gmail.com>
2025-08-06 11:10:36 +0900

    put init url on main instead and fix typos

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import asyncio
 from higisha_touroku_form import higisha_touroku_form
-from constants import BrowswerType
+from constants import BrowserType
 
 
 def main():
     """
         Main function to run the form automation.
     """
-    browser_type = BrowswerType.CHROMIUM
-    asyncio.run(higisha_touroku_form(browser_type))
+    browser_type = BrowserType.CHROMIUM
+    login_url = "http://vm-ryuchiwin2019/ryuchisys/login_dummy/"
+    # URL can be replace to local or domain URL
+    asyncio.run(higisha_touroku_form(login_url, browser_type))
 
 
 if __name__ == "__main__":
